b249a8d05fb738b1717d4c841de5f76c3275c16c
(no author) <(no author)@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2008-09-25 21:05:44 +0000

    removed a old comment

--- app/models/ServiceTypes.php
+++ app/models/ServiceTypes.php
@@ -16,7 +16,6 @@ class ServiceTypes
     public function fetchAll()
     {
         $schema = config()->db_oim_schema;
-        //$sql = "select * from $schema.service s where service_id in (select service_id from $schema.service_service_group g where g.service_group_id = 1)";
         $sql = "SELECT * FROM $schema.service S WHERE S.service_id IN (SELECT service_id FROM $schema.service_service_group WHERE service_group_id=1) AND S.service_id NOT IN (SELECT DISTINCT PS.parent_service_id psid FROM $schema.service PS WHERE PS.parent_service_id IS NOT NULL)";
         return $this->db->fetchAll($sql);
     }
