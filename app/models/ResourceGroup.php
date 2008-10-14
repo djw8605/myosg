0e5cce55d1f742e84eb74425ac4dde97b978de26
(no author) <(no author)@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2008-10-14 20:03:56 +0000

    adjusted

--- app/models/ResourceGroup.php
+++ app/models/ResourceGroup.php
@@ -4,7 +4,7 @@ class ResourceGroup extends CachedIndexedModel
 {
     public function sql($params)
     {
-        $where = "where active = 1 and disable = 0";
+        $where = "where active = 1 and disable = 0 and resource_group_id in (select resource_group_id from oim.resource_resource_group group by resource_group_id)";
         if(isset($params["osg_grid_type_id"])) {
             $where .= " and osg_grid_type_id = ".$params["osg_grid_type_id"];
         }
@@ -12,7 +12,7 @@ class ResourceGroup extends CachedIndexedModel
             $where .= " and resource_group_id = ".$params["resourcegroup"];
         }
         $sql = "SELECT resource_group_id id, name, osg_grid_type_id, description, site_id
-            FROM oim.resource_group $where
+            FROM oim.resource_group rg $where
             order by name";
         return $sql;
     }
