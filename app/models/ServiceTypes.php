cc95d74a2c7065c991f4168a57ea50103b34a1a7
(no author) <(no author)@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2008-10-27 14:23:22 +0000

    pulling all services..

--- app/models/ServiceTypes.php
+++ app/models/ServiceTypes.php
@@ -4,7 +4,8 @@ class ServiceTypes extends CachedIndexedModel
 {
     public function sql($param)
     {
-        return "SELECT * FROM oim.service S WHERE S.service_id IN (SELECT service_id FROM oim.service_service_group WHERE service_group_id=1) AND S.service_id NOT IN (SELECT DISTINCT PS.parent_service_id psid FROM oim.service PS WHERE PS.parent_service_id IS NOT NULL)";
+        return "SELECT * FROM oim.service";
+        //return "SELECT * FROM oim.service S WHERE S.service_id IN (SELECT service_id FROM oim.service_service_group WHERE service_group_id=1) AND S.service_id NOT IN (SELECT DISTINCT PS.parent_service_id psid FROM oim.service PS WHERE PS.parent_service_id IS NOT NULL)";
     }
     public function key() { return "service_id"; }
 }
