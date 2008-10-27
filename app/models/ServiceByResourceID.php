deeab907c799edbc323bc910283a180bba7068cf
(no author) <(no author)@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2008-10-27 14:29:05 +0000

    added filter to servicebyresourceid

--- app/models/ServiceByResourceID.php
+++ app/models/ServiceByResourceID.php
@@ -4,7 +4,10 @@ class ServiceByResourceID extends CachedIndexedModel
 {
     public function sql($param)
     {
-        return "SELECT * from oim.resource_service";
+        return "SELECT * from oim.resource_service ".
+            "where ".
+            "   service_id in (SELECT service_id FROM oim.service_service_group WHERE service_group_id=1) AND ".
+            "   service_id NOT IN (SELECT DISTINCT PS.parent_service_id psid FROM oim.service PS WHERE PS.parent_service_id IS NOT NULL)";
     }
     public function key() { return "resource_id"; }
 }
