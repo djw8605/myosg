81736514b00601c9aa8c8c014ac75a04988a4d5a
(no author) <(no author)@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2008-11-18 18:26:57 +0000

    added last 7 day period

--- app/controls/AandrController.php
+++ app/controls/AandrController.php
@@ -10,6 +10,9 @@ class AandrController extends ControllerBase
 
     public function processPeriodQuery()
     {
+        $start_time = null;
+        $end_time = null;
+
         if(!isset($_REQUEST["period"])) {
             //this causes the graph period combo box to use following as default, as well as telling graph 
             //the default length
@@ -19,28 +22,26 @@ class AandrController extends ControllerBase
         $this->view->period = $dirty_period;
         switch($dirty_period) {
         case "1day":
-            $this->history_days = 1;
-            break;
-        case "3day":
-            $this->history_days = 3;
+            $end_time = (int)(time() / 86400) * 86400;
+            $start_time = $end_time - 86400;
             break;
         case "week":
-            $this->history_days = 7;
-            break;
-        case "month":
-            $this->history_days = 31;
-            break;
-        case "year":
-            $this->history_days = 365;
+            $end_time = (int)(time() / 86400) * 86400;
+            $start_time = $end_time - 86400*7;
             break;
         default:
             throw new exception("bad period: $dirty_period");
         }
+
+        return array($start_time, $end_time);
     }
 
     public function load()
     {
-        $this->processPeriodQuery();
+        //determine report period
+        list($start_time, $end_time) = $this->processPeriodQuery();
+        $this->view->start_time = $start_time;
+        $this->view->end_time = $end_time;
 
         ///////////////////////////////////////////////////////////////////////
         // Load graph inforamtion
@@ -51,11 +52,6 @@ class AandrController extends ControllerBase
                 $resource_id = $dirty_resource_id;
             }
         }
-
-        //determine report period
-        list($start_time, $end_time) = getLastNDayRangeProper($this->history_days);
-        $this->view->start_time = $start_time;
-        $this->view->end_time = $end_time;
 
         //use the same param for both resource / resource service model
         $params = array();
@@ -78,10 +74,16 @@ class AandrController extends ControllerBase
         if(file_exists($cache_filename)) {
             $cache_xml = file_get_contents($cache_filename);
             $aandr = new SimpleXMLElement($cache_xml);
-
-            //filter by resource_id
             $this->view->aandr = array();
+
+            $this->view->calc_time = (int)$aandr->CalculateTimestamp[0];
+
+            //pass A&R Info
             foreach($aandr->Resources[0] as $resource) {
+                //filter by resource_id
+                if($resource_id !== null) {
+                    if($resource_id != (int)$resource->ResourceID) continue;
+                }
                 $this->view->aandr[(int)$resource->ResourceID] = $resource->Services[0];
             }
 
@@ -89,129 +91,4 @@ class AandrController extends ControllerBase
         $this->setpagetitle(AandrController::default_title());
     }
 
-    private function getDowntimesForService($downtimes, $service_id)
-    {
-        $downtime_service_model = new DowntimeService();
-        $downtime_service = $downtime_service_model->get();
-        $downtime_forservice = array();
-
-        foreach($downtimes as $downtime) {
-            $id = $downtime->downtime_id;
-            foreach($downtime_service as $service) {
-                if($service->downtime_id == $id) {
-                    $downtime_forservice[] = $downtime;
-                }
-            }
-        }
-        return $downtime_forservice;
-    }
-
-    function calculateAR($resources, $resource_services, $start_time, $end_time)
-    {
-        $ars = array();
-        
-        foreach($resources as $resource) {
-            $ars[$resource->id] = array();
-
-            foreach($resource_services as $service) {
-                if($service->resource_id == $resource->id) {
-
-                    /////////////////////////////////////////////////////////////////////////////////
-                    //pull status changes
-                    $params = array();
-                    $params["resource_id"] = $resource->id;
-                    $params["start_time"] = $start_time;
-                    $params["end_time"] = $end_time;
-                    $params["service_id"] = $service->service_id;
-                    $service_statuschange_model = new ServiceStatusChange();
-                    $status_changes = $service_statuschange_model->get($params);
-                
-            /*
-                    /////////////////////////////////////////////////////////////////////////////////
-                    //pull downtime info
-                    $downtime_model = new Downtime();
-                    $params = array("resource_id" => $resource_id, "start_time"=>$start_time, "end_time"=>$end_time);
-                    $downtimes = $downtime_model->get($params);
-                    $downtimes_forservice = $this->getDowntimesForService($downtimes, $service_id);
-            */
-
-                    ////////////////////////////////////////////////////////////////////////////////
-                    //calculate AR
-                    $total_time = $end_time - $start_time;
-                    $up_time = 0;
-
-                    $decile_out = 0; //used to calculate the reminder area
-                    $first = true;
-                    if($total_time > 0) {
-                        foreach($status_changes as $change) {
-                            $time = $change->timestamp;
-                            if($first) {
-                                if($time < $start_time) $time = $start_time;
-                                $decile1 = $time - $start_time;
-                                $decile_out = $decile1;
-                                $status = (int)$change->status_id;
-                                $first = false;
-                            } else {
-                                $next_status = (int)$change->status_id;
-                                $decile2 = $time - $start_time;
-                                $size = ($decile2 - $decile1);
-                                if($status == "OK" or $status == "WARNING") {
-                                    $up_time += $size;
-                                }
-                                $decile_out += $size;
-                                $status = $next_status;
-                                $decile1 = $decile2;
-                            }
-                        }
-                        if(count($status_changes) > 0) {
-                            //fill leftover
-                            if($status == "OK" or $status == "WARNING") {
-                                $up_time += $total_time - $decile_out;
-                            }
-                        }
-                    }
-
-                    $down_time = 0;
-            /*
-                    //now draw downtimes
-                    foreach($downtimes as $downtime) {
-                        $start_p = (float)($downtime->unix_start_time-$start_time)/$total_time*$image_width;
-                        $end_p = (float)($downtime->unix_end_time-$start_time)/$total_time*$image_width;
-                        imageline($im, $start_p, 0, $end_p, 0, $color_downtime);
-                    }
-            */
-                    $ars[$resource->id][$service->service_id] = array($total_time, $up_time, $down_time);
-                    break;
-                }
-            }
-        }
-
-        return $ars;
-    }
-
-    private function metric_overwrite($metric, $latest)
-    {
-        //find the update from $latest and apply change to $metric
-        foreach($latest as $latest_metric) {
-            if($latest_metric->metric_id == $metric->MetricID[0]) {
-                $metric->MetricDataID = $latest_metric->id;
-                $metric->Timestamp = $latest_metric->timestamp;
-                $metric->Detail = $this->fetchMetricDetail($latest_metric->id);
-                $metric->Status = Status::getStatus($latest_metric->metric_status_id);
-                return;
-            }
-        }
-        //didn't find the match - clear it
-        $metric->MetricDataID = null;
-        $metric->Timestamp = null;
-        $metric->Detail = null;
-        $metric->Status = null;
-    }
-    private function fetchMetricDetail($id)
-    {
-        static $metric_detail_model = null;
-        if($metric_detail_model === null) $metric_detail_model = new MetricDetail();
-        $detail = $metric_detail_model->get(array("id"=>$id)); 
-        return $detail[0]->detail; 
-    }
 }
--- app/timerange.php
+++ app/timerange.php
@@ -7,11 +7,7 @@ function getLastNDayRange($nday = 1)
 
 function getLastNDayRangeProper($nday = 1)
 {
-    $d_day = date('j', time());
-    $d_month = date('n', time());
-    $d_year = date('Y', time());
-    $today = mktime(0,0,0,$d_month, $d_day, $d_year);
-    $yesterday = $today - 3600*24*$nday;
-
-    return array($yesterday, $today);
+    $today = (int)(time() / 86400) * 86400;
+    $before = $today - 3600*24*$nday;
+    return array($before, $today);
 }
