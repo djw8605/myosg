bbd897c7433d1fa63cfe818c8e695849f44ccd27
(no author) <(no author)@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2008-11-19 01:56:00 +0000

    added feedback feature, and re-added urchin tracker

--- app/models/CachedModel.php
+++ app/models/CachedModel.php
@@ -10,7 +10,7 @@ abstract class CachedModel extends Model
 
         $str_param = print_r($params, true);
         if(!isset(CachedModel::$cache[get_class($this)][$str_param])) {
-            dlog(get_class($this)." // callin load with ".print_r($this->params, true));
+            //dlog(get_class($this)." // callin load with ".print_r($this->params, true));
             CachedModel::$cache[get_class($this)][$str_param] = $this->load($params);
         }
         return CachedModel::$cache[get_class($this)][$str_param];
