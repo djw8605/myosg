3332a613e15cd910409cfacca9fd660dfde0f8c5
(no author) <(no author)@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2008-12-16 19:09:56 +0000

    Added where timestamp < time() clause to filter status changes occures in the future - This could legitimatly happen if a new metric is entered close to the current timestamp

--- app/models/LatestResourceStatus.php
+++ app/models/LatestResourceStatus.php
@@ -2,7 +2,9 @@
 class LatestResourceStatus extends Model
 {
     public function sql($params) {
-        $sql = "SELECT * FROM statuschange_resource s group by resource_id having max(timestamp)";
+        $time = time();
+        $sql = "select a.* from statuschange_resource a, (SELECT resource_id, max(timestamp) timestamp FROM statuschange_resource where timestamp < $time group by resource_id) b where a.resource_id = b.resource_id and a.timestamp = b.timestamp";
+        dlog($sql);
         return $sql;
     }
 }
