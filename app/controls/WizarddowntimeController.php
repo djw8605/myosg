88853451c3eaf4686c75be8d84d675fbb40f0565
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-02-26 18:44:07 +0000

    Re-orged control and view code for downtime, and addes XML view

--- app/controls/WizarddowntimeController.php
+++ app/controls/WizarddowntimeController.php
@@ -12,24 +12,66 @@ class WizarddowntimeController extends WizardController
         
         $this->view->resource_ids = $this->resource_ids;
 
-        $resource_model = new Resource();
-        $this->view->resources = $resource_model->getindex();
-
+        //pull current downtimes
         $downtime_model = new Downtime();
         $today_start = time()/(3600*24) * (3600*24);
         $today_end = time()/(3600*24) * (3600*24) + 3600*24;
+        $this->view->current_downtimes = $this->formatInfo($downtime_model->getCurrentDowntimes($today_start, $today_end));
 
-        $this->view->current_downtimes = $downtime_model->getCurrentDowntimes($today_start, $today_end);
-        $this->view->future_downtimes = $downtime_model->getFutureDowntimes(time());
-
-        $model = new ServiceTypes();
-        $this->view->service_info = $model->getindex();
-
-        dlog($this->view->service_info);
-
-        $downtime_service_model = new DowntimeService();
-        $this->view->downtime_services = $downtime_service_model->get();
+        //pull future downtimes
+        $this->view->future_downtimes = $this->formatInfo($downtime_model->getFutureDowntimes(time()));
 
         $this->setpagetitle(self::default_title());
     }
+
+    function formatInfo($downtime_recs)
+    {
+        $downtimes = array();
+
+        $resource_model = new Resource();
+        $resources = $resource_model->getindex();
+
+        $downtime_service_model = new DowntimeService();
+        $downtime_services = $downtime_service_model->get();
+
+        $model = new ServiceTypes();
+        $service_info = $model->getindex();
+
+        foreach($downtime_recs as $downtime_a)
+        {
+            $downtime = $downtime_a[0];
+            if(in_array($downtime->resource_id, $this->resource_ids)) {
+                //only show event that we have pulled resource for
+                $resource = $resources[$downtime->resource_id];
+                $resource_name = $resource[0]->name;
+                if($resource_name !== null) {
+
+                    $start = date(config()->date_format_full, $downtime->unix_start_time);
+                    $end = date(config()->date_format_full, $downtime->unix_end_time);
+
+                    //get affected services
+                    $affected_services = array();
+                    foreach($downtime_services as $service) {
+                        if($service->downtime_id == $downtime->downtime_id) {
+                            $info = $service_info[$service->service_id][0];
+                            $affected_services[] = $info->description;
+                        }
+                    }
+
+                    $desc = $downtime->downtime_summary;
+                    $desc = str_replace(array("\n", "\r"), "", $desc);
+
+                    $downtimes[] = array("id"=>$downtime->resource_id, 
+                        "name"=>$resource_name,
+                        "desc"=>$desc,
+                        "services"=>$affected_services,
+                        "start_time"=>$start,
+                        "end_time"=>$end
+                    );
+                }
+            }
+        }
+        return $downtimes;
+    }
+    
 }
