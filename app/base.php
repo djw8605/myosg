3cd2358a8db69ed5143dedbd17b5d2061f8ebc94
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-03-24 13:28:13 +0000

    Fixed the global removeslash function for array element

--- app/base.php
+++ app/base.php
@@ -14,10 +14,24 @@ function greet()
 function remove_quotes()
 {
     if(  ( function_exists("get_magic_quotes_gpc") && get_magic_quotes_gpc() ) || ini_get('magic_quotes_sybase')  ){
-        foreach($_GET as $k => $v) $_GET[$k] = stripslashes($v);
-        foreach($_POST as $k => $v) $_POST[$k] = stripslashes($v);
-        foreach($_COOKIE as $k => $v) $_COOKIE[$k] = stripslashes($v);
-        foreach($_REQUEST as $k => $v) $_REQUEST[$k] = stripslashes($v);
+        foreach($_GET as $k => $v) $_GET[$k] = stripslashes_deep($v);
+        foreach($_POST as $k => $v) $_POST[$k] = stripslashes_deep($v);
+        foreach($_COOKIE as $k => $v) $_COOKIE[$k] = stripslashes_deep($v);
+        foreach($_REQUEST as $k => $v) $_REQUEST[$k] = stripslashes_deep($v);
+    }
+}
+
+
+function stripslashes_deep($v)
+{
+    if(is_array($v)) {
+        $ret = array();
+        foreach($v as $key=>$item) {
+            $ret[$key] = stripslashes_deep($item);
+        }
+        return $ret;
+    } else {
+        return stripslashes($v);
     }
 }
 
@@ -40,5 +54,29 @@ function sendSMS($users, $subject, $body)
     mail($recipient, $subject, $body, $header);
 
     dlog("Sent SMS notification to $recipient user:".print_r($users, true));
+}
+
+function signedmail($to, $subject, $body, $header = "")
+{
+    $key = config()->signed_email_key;
+    $cert = config()->signed_email_cert;
+
+    //store the original body (so that openssl can process it)
+    $original_body = tempnam("/tmp", "gocticket");
+    $handle = fopen($original_body, "w");
+    fwrite($handle, $body);
+    fclose($handle);
+
+    //sign the body
+    $signed_body = tempnam("/tmp", "gocticket");
+    system("openssl smime -sign -text -inkey $key -signer $cert -in $original_body | dos2unix > $signed_body");
+
+    //insert the signed content and my header to $header
+    $header .= "\r\nFrom: " . config()->signed_email_from."\r\n";
+    $header .= file_get_contents($signed_body);
+
+    //send everything from $header
+    slog($header);
+    return mail($to, $subject, "", $header);
 }
 
