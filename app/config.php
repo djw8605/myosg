aa70fca1ce5d166c081d340bbeb618982cc71ecf
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-05-22 03:48:30 +0000

    Incremented version number

--- app/config.php
+++ app/config.php
@@ -47,7 +47,7 @@ class common_config
         //
 
         //application version to use for version specific data structures, etc.
-        $this->version = "1.1";
+        $this->version = "1.2";
         //application to display or used in email
         $this->app_name = "MyOSG";
         $this->app_subname = "by Grid Operations Center";
