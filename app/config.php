857296f9482143dec63cbd0da1f8eb1a9fff27ea
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-06-25 16:51:08 +0000

    Changed version number from 1.5 to 1.4.1

--- app/config.php
+++ app/config.php
@@ -47,7 +47,7 @@ class common_config
         //
 
         //application version to use for version specific data structures, etc.
-        $this->version = "1.5";
+        $this->version = "1.4.1";
         //application to display or used in email
         $this->app_name = "MyOSG";
         $this->app_subname = "by Grid Operations Center";
