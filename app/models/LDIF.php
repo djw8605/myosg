8c5dc67c896a4aa70249755622ab25309e1c6741
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2009-06-30 13:25:26 +0000

    Updated LDIF to handle missing GIP XML

--- app/models/LDIF.php
+++ app/models/LDIF.php
@@ -12,29 +12,37 @@ class LDIF
         $gip = new SimpleXMLElement($cache_xml);
 
         //load ITB gip summary (and merge!)
-        if(!file_exists(config()->gip_summary_itb)) {
-            throw new exception("Can't find the gip summary xml: ".config()->gip_summary_itb);
+        if(file_exists(config()->gip_summary_itb)) {
+            $cache_xml_itb = file_get_contents(config()->gip_summary_itb);
+            $this->merge($gip, new SimpleXMLElement($cache_xml_itb));
+        } else {
+            elog("Can't find the gip summary xml (ignoring): ".config()->gip_summary_itb);
         }
-        $cache_xml_itb = file_get_contents(config()->gip_summary_itb);
-        $this->merge($gip, new SimpleXMLElement($cache_xml_itb));
         return $gip;
     }
 
     public function getBdii()
     {
+        //try only for 1 seconds to pull this data
+        $ctx = stream_context_create(array( 'http' => array('timeout' => 2)));
+
         //cemon raw file listing for production
-        $cemonbdii_url = file_get_contents(config()->cemonbdii_url);
+        slog("loading ".config()->cemonbdii_url);
+        $cemonbdii_url = file_get_contents(config()->cemonbdii_url, 0, $ctx);
+
         $cemonbdii = new SimpleXMLElement($cemonbdii_url);
 
         //cemon raw file listing for itb
-	$cemonbdii_itb_url = file_get_contents(config()->cemonbdii_itb_url);
-	try {
-		$itb = new SimpleXMLElement($cemonbdii_itb_url);
-		$this->merge($cemonbdii, $itb);
-	} catch(exception $e) {
-		elog("failed to parse for some reason... maybe itb is not available?");
-	}
-	return $cemonbdii;
+        slog("loading ".config()->cemonbdii_itb_url);
+        $cemonbdii_itb_url = file_get_contents(config()->cemonbdii_itb_url, 0, $ctx);
+
+        try {
+                $itb = new SimpleXMLElement($cemonbdii_itb_url);
+                $this->merge($cemonbdii, $itb);
+        } catch(exception $e) {
+                elog("failed to parse for some reason... maybe itb is not available?");
+        }
+        return $cemonbdii;
     }
 
     function merge(SimpleXMLElement &$xml1, SimpleXMLElement $xml2)
