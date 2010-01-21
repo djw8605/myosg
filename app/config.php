b89207800d5ac41b91ad7028a4ffd18cd309dbb0
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2010-01-21 02:08:16 +0000

    Added ref parameter for report bug link

--- app/config.php
+++ app/config.php
@@ -251,7 +251,12 @@ function fullbase($p = "http") {
     if($p == "http") {
         if(isset($_SERVER["HTTPS"])) $p = "https";
     }
-
     return $p."://".$_SERVER["HTTP_HOST"].base();
+}
+function fullurl() {
+    $s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : ""; $protocol = strleft(strtolower($_SERVER["SERVER_PROTOCOL"]), "/").$s; $port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]); return $protocol."://".$_SERVER['SERVER_NAME'].$port.$_SERVER['REQUEST_URI'];
+}
+function strleft($s1, $s2) {
+    return substr($s1, 0, strpos($s1, $s2));
 }
 
