9cf1a0fac645a091a3395f350fdbb45a3b49b3bf
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2011-09-21 13:29:19 +0000

    1.41 begins (patched) fixed the kml icon issue

--- app/config.php
+++ app/config.php
@@ -61,7 +61,7 @@ class common_config
         //
 
         //application version to use for version specific data structures, etc.
-        $this->version = "1.40";
+        $this->version = "1.41";
 
         //application to display or used in email
         $this->app_name = "MyOSG";
