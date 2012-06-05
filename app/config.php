711cd85be5f59a7fa2d8d7f39bb8476ea6bf45b8
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2012-06-05 14:04:56 +0000

    1.52 begins

--- app/config.php
+++ app/config.php
@@ -61,7 +61,7 @@ class common_config
         //
 
         //application version to use for version specific data structures, etc.
-        $this->version = "1.51";
+        $this->version = "1.52";
 
         //application to display or used in email
         $this->app_name = "MyOSG";
