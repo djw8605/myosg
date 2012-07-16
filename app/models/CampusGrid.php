a13fb670bda9c4283714baeb0bbdcdd3300dec74
/DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343 </DC=org/DC=doegrids/OU=People/CN=Soichi Hayashi 461343@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2012-07-16 13:01:52 +0000

    added descriptions to remaining campus grid sites

--- app/models/CampusGrid.php
+++ app/models/CampusGrid.php
@@ -20,19 +20,19 @@ class CampusGrid
     public function getIcons() {
         return array(
             array("name"=>"UF", "lat"=>29.681312, "lon"=>-82.5, "icon"=>"images/campus/uf.png", 
-                "desc"=>""),
+                "desc"=>"The SSERCA (Sunshine State Education and Research Computing Alliance) Grid is a proposed state-wide distributed computing environment targeted to share resources at different campuses to meet the scientific computing needs of the public and private member universities in Florida."),
 
             array("name"=>"UCSD", "lat"=>33.016928, "lon"=>-116.846046, "icon"=>"images/campus/ucsd.png", 
-                "desc"=>"UCSD stuff here.."),
+                "desc"=>"The particle physics group enables scientists in Astronomy, Biology, Chemistry, Climate Science, Engineering, Oceanography, Pharmacology, and Physics to use computing resources across campus and beyond."),
 
             array("name"=>"PU", "lat"=>40.424923, "lon"=>-87.0, "icon"=>"images/campus/pu.png", 
                 "desc"=>"Researchers and educators at Purdue University-West Lafayette (Purdue), Purdue University-Calumet (Calumet), and the University of Notre Dame."),
 
             array("name"=>"Fermilab", "lat"=>41.85143, "lon"=>-88.242767, "icon"=>"images/campus/fermilab.gif",
-                "desc"=>""),
+                "desc"=>"FermiGrid (The Fermilab Campus Grid) integrates clusters at FNAL in support of the Energy, Cosmic and Intensity Frontier Experiments."),
 
             array("name"=>"Virginia Tech", "lat"=>37.221196, "lon"=>-80.426488, "icon"=>"images/campus/vt.png",
-                "desc"=>""),
+                "desc"=>"The Virginia Tech (VT) Campus Grid is housed at the Virginia Bioinformatics Institute (VBI) and supports VBI laboratory groups as well as the broader life sciences research community at VT."),
 
             array("name"=>"Nebraska", "lat"=>40.819818, "lon"=>-96.705651, "icon"=>"images/campus/n.png",
                 "desc"=>"HCC supports NU faculty and staff from a variety of fields and 4 campuses located at Lincoln, Omaha and Kearney."),
