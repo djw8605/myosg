90fbe757d9c52e4fd9c9468ab14f977ee17b52ef
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Soichi Hayashi 238 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Soichi Hayashi 238@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2013-05-07 13:08:49 +0000

    2.11 begins (patched) updated supported vo label text color to white

--- app/config.php
+++ app/config.php
@@ -61,7 +61,7 @@ class common_config
         //
 
         //application version to use for version specific data structures, etc.
-        $this->version = "2.10";
+        $this->version = "2.11";
 
         //application to display or used in email
         $this->app_name = "MyOSG";
