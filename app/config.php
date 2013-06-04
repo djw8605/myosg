1254149ba80bf5020cefca46e9054f7e7199d184
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Soichi Hayashi 238 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Soichi Hayashi 238@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2013-06-04 16:24:32 +0000

    2.12 begins

--- app/config.php
+++ app/config.php
@@ -61,7 +61,7 @@ class common_config
         //
 
         //application version to use for version specific data structures, etc.
-        $this->version = "2.11.2";
+        $this->version = "2.12";
 
         //application to display or used in email
         $this->app_name = "MyOSG";
