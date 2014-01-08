68b7ae5d7ff7905b6830621291045b74e02c6767
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Soichi Hayashi 238 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Soichi Hayashi 238@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2014-01-08 15:51:30 +0000

    filtered out disabled contact on vooasisuser

--- app/models/VOOasisUser.php
+++ app/models/VOOasisUser.php
@@ -27,7 +27,7 @@ class VOOasisUser extends CachedIndexedModel
 
         $sql = "SELECT dn.dn_string AS dn, rc.*, c.* FROM vo_oasis_user rc ".
                 "JOIN contact c ON rc.contact_id = c.id ".
-                "LEFT join dn ON dn.contact_id = c.id WHERE dn.disable = 0 $where";
+                "LEFT join dn ON dn.contact_id = c.id WHERE dn.disable = 0 and c.disable = 0 $where";
         return $sql;
     }
     public function key() { return "vo_id"; }
