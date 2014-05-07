42ba567479bcf5dab94262621176832b971b0812
/DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Soichi Hayashi 238 </DC=com/DC=DigiCert-Grid/O=Open Science Grid/OU=People/CN=Soichi Hayashi 238@ee955d64-9ebb-4cd7-93a8-b3fd85ff1eb3>
2014-05-07 19:26:08 +0000

    (related to the previous commit) added conversion code for old 2 new format used by rgcurrentstatus (current_status_attrs_spe_sel) fixed css issue on chrome with bdii browser

--- app/controls/RgcurrentstatusController.php
+++ app/controls/RgcurrentstatusController.php
@@ -28,6 +28,18 @@ class RgcurrentstatusController extends RgController
         $model = new ResourceGroup();
         $this->view->resource_groups = $model->getindex();
 
+        //convert old current_status_attrs_spe format to the new
+        if(isset($_REQUEST["current_status_attrs_spe"]) && !isset($_REQUEST["current_status_attrs_spe_sel"])) {
+            $ids = array();
+            foreach($_REQUEST as $key=>$value) {
+                if(strpos($key, "current_status_attrs_spe_") === 0) {
+                    $id = substr($key, strlen("current_status_attrs_spe_"));
+                    $ids[] = $id;
+                }
+            }
+            $_REQUEST["current_status_attrs_spe_sel"] = $ids;
+        }
+
         //load current status cache for all requested resources
         $this->view->cache = array();
         foreach($this->view->rgs as $rgid=>$rg) {
