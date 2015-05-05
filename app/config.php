3a34a5a0240a126a46799c033d3086ad056d1e90
Soichi Hayashi <soichih@gmail.com>
2015-05-05 13:25:06 +0000

    2.35 begins

--- app/config.php
+++ app/config.php
@@ -61,7 +61,7 @@ class common_config
         //
 
         //application version to use for version specific data structures, etc.
-        $this->version = "2.34";
+        $this->version = "2.35";
 
         //application to display or used in email
         $this->app_name = "MyOSG";
