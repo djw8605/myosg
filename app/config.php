63290194d5b951412f4185a135167bf91034221e
schmiecs <schmiecs@sarahs-mbp>
2017-09-18 11:02:20 -0400

    Scott's changes for the map, meshconfig rules in htaccess, version number

--- app/config.php
+++ app/config.php
@@ -61,7 +61,7 @@ class common_config
         //
 
         //application version to use for version specific data structures, etc.
-        $this->version = "2.38";
+        $this->version = "2.45";
 
         //application to display or used in email
         $this->app_name = "MyOSG";
